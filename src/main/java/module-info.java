340b820aaa4d430dcaff8983e7260ef09760fac8
gaveinti09 <gaveinti@espol.edu.ec>
2021-08-12 12:02:26 -0500

    Reinicio de repo

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module ec.edu.espol.chanton_proyecto {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-    //requires java.base;
-
-    opens ec.edu.espol.chanton_proyecto to javafx.fxml;
-    exports ec.edu.espol.chanton_proyecto;
-}
