39e810442d2cca37745f9f1483130045afb89fa6
gaveinti09 <gaveinti@espol.edu.ec>
2021-08-11 22:07:28 -0500

    Intentando hacer que corra

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module ec.edu.espol.chanton_proyecto {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    //requires java.base;
 
     opens ec.edu.espol.chanton_proyecto to javafx.fxml;
     exports ec.edu.espol.chanton_proyecto;
