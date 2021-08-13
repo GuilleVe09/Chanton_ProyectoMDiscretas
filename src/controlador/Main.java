138cece442f1697eb60e920244847b6456a0b62d
gaveinti09 <gaveinti@espol.edu.ec>
2021-08-13 00:49:00 -0500

    Probando y agregando numero de rondas

--- src/controlador/Main.java
+++ src/controlador/Main.java
@@ -26,11 +26,11 @@ public class Main extends Application{
         
         try{
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Main.class.getResource("/vistas/ej.fxml"));
+            loader.setLocation(Main.class.getResource("/vistas/pantallaInicial.fxml"));
             Pane ventana = (Pane) loader.load();
             
             Scene scene = new Scene(ventana);
-            //scene.setRoot(ventana);
+            scene.setRoot(ventana);
             primaryStage.setScene(scene);
             primaryStage.show();
         }catch(IOException e){
